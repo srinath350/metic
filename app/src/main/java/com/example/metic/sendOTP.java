0fe8031971e560d6850515d81f928364656eefa5
srinath350 <srinathr350@gmail.com>
2022-05-17 13:01:14 +0530

    trial

--- app/src/main/java/com/example/metic/sendOTP.java
+++ app/src/main/java/com/example/metic/sendOTP.java
@@ -3,6 +3,7 @@ package com.example.metic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class sendOTP extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class sendOTP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_otp);
+
+        Toast.makeText(this, "Bleh", Toast.LENGTH_SHORT).show();
     }
 }
