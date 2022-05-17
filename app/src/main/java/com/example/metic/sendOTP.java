687eb6b4a4f2f9a605e22abfa4bed63cfacac343
srinath350 <srinathr350@gmail.com>
2022-05-17 12:57:39 +0530

    initial commit

---
+++ app/src/main/java/com/example/metic/sendOTP.java
@@ -0,0 +1,14 @@
+package com.example.metic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sendOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_otp);
+    }
+}
